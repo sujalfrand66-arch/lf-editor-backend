0f186084983d9bd611fab4ecd97043a73832d3f7
sujalfrand66-arch <sujalfrand66@gmail.com>
2026-02-06 15:23:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-const app = require('./app/app');
+const app = require('.app.js);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server running on port ${PORT}`);
 });
+
